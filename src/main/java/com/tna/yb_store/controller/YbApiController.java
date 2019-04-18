9708074524c8ba7cbd10f4d8124d25b6a9df65a5
YPXWYT <2568824264@qq.com>
2019-04-18 22:37:03 +0800

    YbStore

--- src/main/java/com/tna/yb_store/controller/YbApiController.java
+++ src/main/java/com/tna/yb_store/controller/YbApiController.java
@@ -2,17 +2,26 @@ package com.tna.yb_store.controller;
 
 import cn.yiban.open.Authorize;
 import cn.yiban.open.common.User;
+import com.tna.yb_store.entity.BaseResponseBody;
+import com.tna.yb_store.entity.Product;
+import com.tna.yb_store.entity.TokenResponseBody;
+import com.tna.yb_store.entity.YbUser;
+import com.tna.yb_store.service.OrderService;
+import com.tna.yb_store.service.ProductService;
 import com.tna.yb_store.utils.AppContext;
 import com.tna.yb_store.utils.redis.RedisUtil;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -21,15 +30,17 @@ public class YbApiController {
 
     @Resource
     private RedisUtil redisUtil;
+
     public void setRedisUtil(RedisUtil redisUtil) {
         this.redisUtil = redisUtil;
     }
 
     @Resource
-    RedisTemplate<Object, Object> redisTemplate;
+    private ProductService productService;
+
 
     @RequestMapping("/authorize")
-    public void getCode(HttpServletRequest request, HttpServletResponse response)throws Exception{
+    public void getCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Authorize authorize = new Authorize(AppContext.APPID, AppContext.APPSECRET);
         String url = authorize.forwardurl(AppContext.BACKURL, "QUERY", Authorize.DISPLAY_TAG_T.WEB);
         //request.getRequestDispatcher(url).forward(request,response);//此方法无法访问
@@ -38,58 +49,84 @@ public class YbApiController {
 
     @RequestMapping("/access_token")
     @Cacheable("productCache")
-    public String getToken(HttpServletRequest request, HttpServletResponse response)throws Exception{
+    public ResponseEntity<?> getToken(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+        String msg, status;
         String code = request.getParameter("code");
-        if (code == null || code.isEmpty())
-        {
-            response.sendRedirect("index.html");
+        TokenResponseBody tokenResponseBody = new TokenResponseBody();
+
+        if (code == null || code.isEmpty()) {
+            msg = "code为null,授权信息错误！";
+            status = "error";
+            BaseResponseBody baseResponseBody = new BaseResponseBody();
+            baseResponseBody.setMessage(msg);
+            baseResponseBody.setStatus(status);
+            return ResponseEntity.ok(baseResponseBody);
         }
 
-        Authorize auth = new Authorize(AppContext.APPID,AppContext.APPSECRET);
-        String queryTokenRS = auth.querytoken(code,AppContext.BACKURL);
+        Authorize auth = new Authorize(AppContext.APPID, AppContext.APPSECRET);
+        String queryTokenRS = auth.querytoken(code, AppContext.BACKURL);
 
 
-        if (queryTokenRS == null || queryTokenRS.isEmpty())
-        {
-            response.sendRedirect("index.html");
+        if (queryTokenRS == null || queryTokenRS.isEmpty()) {
+            msg = "queryTokenRS为null,授权信息错误！";
+            status = "error";
+            BaseResponseBody baseResponseBody = new BaseResponseBody();
+            baseResponseBody.setMessage(msg);
+            baseResponseBody.setStatus(status);
+            return ResponseEntity.ok(baseResponseBody);
         }
         JSONObject json = JSONObject.fromObject(queryTokenRS);
 
-        if (json == null)
-        {
-            response.sendRedirect("index.html");
+        if (json == null) {
+            msg = "json为null,json转换错误！";
+            status = "error";
+            BaseResponseBody baseResponseBody = new BaseResponseBody();
+            baseResponseBody.setMessage(msg);
+            baseResponseBody.setStatus(status);
+            return ResponseEntity.ok(baseResponseBody);
         }
 
-        if (Objects.requireNonNull(json).has("access_token"))
-        {
-
+        if (Objects.requireNonNull(json).has("access_token")) {
 
             String accessToken = json.getString("access_token");
-            redisUtil.set("access_token",accessToken);
+            redisUtil.set("access_token", accessToken);
 
-            System.out.println(accessToken);
-
-//            授权查询
-            String text1 = auth.getManInstance(accessToken).query();
-            System.out.println(text1);
+            //授权查询
+//                String text1 = auth.getManInstance(accessToken).query();
+//                System.out.println(text1);
 
             User user = new User(accessToken);
+            YbUser ybUser = new YbUser();
+
             JSONObject userInfo = JSONObject.fromObject(user.me()).getJSONObject("info");
-            JSONObject otherUserInfo = JSONObject.fromObject(user.other(2));
+//            JSONObject otherUserInfo = JSONObject.fromObject(user.other(2));
 
-//            Friend friend = new Friend(accessToken);
-//            JSONObject me_list = JSONObject.fromObject(friend.list(1,15));
-//
-//            System.out.println(otherUserInfo);
-//            System.out.println(userInfo);
-//            System.out.println(me_list);
+            ybUser.setYb_userid(userInfo.getString("yb_userid"));
+            ybUser.setYb_username(userInfo.getString("yb_username"));
+            ybUser.setYb_usernick(userInfo.getString("yb_usernick"));
+            ybUser.setYb_sex(userInfo.getString("yb_sex"));
+            ybUser.setYb_money(userInfo.getString("yb_money"));
+            ybUser.setYb_exp(userInfo.getString("yb_exp"));
+            ybUser.setYb_userhead(userInfo.getString("yb_userhead"));
+            ybUser.setYb_schoolname(userInfo.getString("yb_schoolname"));
+            ybUser.setYb_schoolid(userInfo.getString("yb_schoolid"));
+            ybUser.setYb_regtime(userInfo.getString("yb_regtime"));
 
+            List<Product> products = productService.findProductAll();
+            tokenResponseBody.setToken(accessToken);
+            tokenResponseBody.setUser(ybUser);
+            tokenResponseBody.setInfo(products);
+            tokenResponseBody.setMessage("成功获取用户信信息、授权信息及产品信息");
+            tokenResponseBody.setStatus("success");
+        } else {
+            msg = "获取token信息错误！";
+            status = "error";
+            BaseResponseBody baseResponseBody = new BaseResponseBody();
+            baseResponseBody.setMessage(msg);
+            baseResponseBody.setStatus(status);
+            return ResponseEntity.ok(baseResponseBody);
         }
-        else
-        {
-            System.out.println("failed");
-        }
-        return "ok";
+        return ResponseEntity.ok(tokenResponseBody);
     }
 }
