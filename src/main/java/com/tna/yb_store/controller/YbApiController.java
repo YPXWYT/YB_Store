e5125566a7bda6a3cc81057bc3e952af2f13f4a9
YPXWYT <2568824264@qq.com>
2019-04-22 23:28:28 +0800

    YbStore

--- src/main/java/com/tna/yb_store/controller/YbApiController.java
+++ src/main/java/com/tna/yb_store/controller/YbApiController.java
@@ -1,19 +1,13 @@
 package com.tna.yb_store.controller;
 
 import cn.yiban.open.Authorize;
-import cn.yiban.open.common.User;
 import com.tna.yb_store.entity.BaseResponseBody;
-import com.tna.yb_store.entity.Product;
 import com.tna.yb_store.entity.TokenResponseBody;
-import com.tna.yb_store.entity.YbUser;
-import com.tna.yb_store.service.OrderService;
 import com.tna.yb_store.service.ProductService;
 import com.tna.yb_store.utils.AppContext;
 import com.tna.yb_store.utils.redis.RedisUtil;
 import net.sf.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -43,7 +36,6 @@ public class YbApiController {
     public void getCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Authorize authorize = new Authorize(AppContext.APPID, AppContext.APPSECRET);
         String url = authorize.forwardurl(AppContext.BACKURL, "QUERY", Authorize.DISPLAY_TAG_T.WEB);
-        //request.getRequestDispatcher(url).forward(request,response);//此方法无法访问
         response.sendRedirect(url);
     }
 
@@ -92,33 +84,36 @@ public class YbApiController {
             String accessToken = json.getString("access_token");
             redisUtil.set("access_token", accessToken);
 
+            String url = "http://127.0.0.1:3000?token=" +
+                    accessToken;
+            response.sendRedirect(url);
             //授权查询
 //                String text1 = auth.getManInstance(accessToken).query();
 //                System.out.println(text1);
 
-            User user = new User(accessToken);
-            YbUser ybUser = new YbUser();
+//            User user = new User(accessToken);
+//            YbUser ybUser = new YbUser();
 
-            JSONObject userInfo = JSONObject.fromObject(user.me()).getJSONObject("info");
+//            JSONObject userInfo = JSONObject.fromObject(user.me()).getJSONObject("info");
 //            JSONObject otherUserInfo = JSONObject.fromObject(user.other(2));
 
-            ybUser.setYb_userid(userInfo.getString("yb_userid"));
-            ybUser.setYb_username(userInfo.getString("yb_username"));
-            ybUser.setYb_usernick(userInfo.getString("yb_usernick"));
-            ybUser.setYb_sex(userInfo.getString("yb_sex"));
-            ybUser.setYb_money(userInfo.getString("yb_money"));
-            ybUser.setYb_exp(userInfo.getString("yb_exp"));
-            ybUser.setYb_userhead(userInfo.getString("yb_userhead"));
-            ybUser.setYb_schoolname(userInfo.getString("yb_schoolname"));
-            ybUser.setYb_schoolid(userInfo.getString("yb_schoolid"));
-            ybUser.setYb_regtime(userInfo.getString("yb_regtime"));
-
-            List<Product> products = productService.findProductAll();
-            tokenResponseBody.setToken(accessToken);
-            tokenResponseBody.setUser(ybUser);
-            tokenResponseBody.setInfo(products);
-            tokenResponseBody.setMessage("成功获取用户信信息、授权信息及产品信息");
-            tokenResponseBody.setStatus("success");
+//            ybUser.setYb_userid(userInfo.getString("yb_userid"));
+//            ybUser.setYb_username(userInfo.getString("yb_username"));
+//            ybUser.setYb_usernick(userInfo.getString("yb_usernick"));
+//            ybUser.setYb_sex(userInfo.getString("yb_sex"));
+//            ybUser.setYb_money(userInfo.getString("yb_money"));
+//            ybUser.setYb_exp(userInfo.getString("yb_exp"));
+//            ybUser.setYb_userhead(userInfo.getString("yb_userhead"));
+//            ybUser.setYb_schoolname(userInfo.getString("yb_schoolname"));
+//            ybUser.setYb_schoolid(userInfo.getString("yb_schoolid"));
+//            ybUser.setYb_regtime(userInfo.getString("yb_regtime"));
+//
+//            List<Product> products = productService.findProductAll();
+//            tokenResponseBody.setToken(accessToken);
+//            tokenResponseBody.setUser(ybUser);
+//            tokenResponseBody.setInfo(products);
+//            tokenResponseBody.setMessage("成功获取用户信信息、授权信息及产品信息");
+//            tokenResponseBody.setStatus("success");
         } else {
             msg = "获取token信息错误！";
             status = "error";
