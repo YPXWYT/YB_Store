18a3d779dfc6ef4070f6199634f2811e2bb9d98d
YPXWYT <2568824264@qq.com>
2019-04-23 19:12:00 +0800

    YbStore

---
+++ src/main/java/com/tna/yb_store/interceptor/Interceptor1.java
@@ -0,0 +1,42 @@
+package com.tna.yb_store.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Interceptor1 implements HandlerInterceptor {
+
+
+    /**
+     * 在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println("************BaseInterceptor1 afterCompletion executed**********");
+    }
+
+    /**
+     * 请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
+     */
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println("************BaseInterceptor1 postHandle executed**********");
+    }
+
+
+    /**
+     *  在请求处理之前进行调用（Controller方法调用之前）
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+
+        System.out.println("************BaseInterceptor1 preHandle executed**********");
+        return true;
+    }
+}
---
+++ src/main/java/com/tna/yb_store/interceptor/MainInterceptor.java
@@ -0,0 +1,38 @@
+package com.tna.yb_store.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainInterceptor implements HandlerInterceptor {
+
+//    后处理
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println("************BaseInterceptor afterCompletion executed**********");
+    }
+
+//    返回处理
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println("************BaseInterceptor postHandle executed**********");
+    }
+
+    //    预处理
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
+        System.out.println("************BaseInterceptor preHandle executed**********");
+        return true;
+    }
+}
